0dc445551262605442022240fc80570e9b69a408
Marco Boucas <marco.boucas@hotmail.fr>
2022-01-26 19:24:06 +0100

    Add the ability to parse a list

--- dprint/typeprint.py
+++ dprint/typeprint.py
@@ -1,16 +1,66 @@
 """TypePrint."""
 
+from collections import defaultdict
 from typing import Any
 
+PADDING = 2
 
-def typeprint_str(element: Any) -> str:
+
+def add_padding(text: str, current_padding: str) -> str:
+    """Add padding and merge."""
+    return (
+        "\n".join(
+            list(
+                map(
+                    lambda x: current_padding + x.strip("\n"),
+                    text.strip("\n").split("\n"),
+                )
+            )
+        )
+        + "\n"
+    )
+
+
+def get_type(ele: Any) -> str:
+    """Get the type of an element."""
+    return str(type(ele)).split("'")[1]
+
+
+def typeprint_str(
+    element: Any, current_padding: str = "", padding_increment: int = PADDING
+) -> str:
     """Typeprint an element to string."""
-    type_ = str(type(element)).split("'")[1]
+    type_ = get_type(element)
     if isinstance(element, (str, int, float)):
-        return type_
+        return add_padding(type_, current_padding)
+    if isinstance(element, list):
+
+        # Empty list
+        if len(element) == 0:
+            return add_padding("List[]", current_padding)
+
+        # If not, we count the number of elements that differ (in terms of type)
+        counter = defaultdict(list)
+        for ele in element:
+            counter[get_type(ele)].append(ele)
+        # Sort by type value (to get unique order)
+        counter_values = list(
+            map(lambda x: x[1], sorted(list(counter.items()), key=lambda x: x[0]))
+        )
+        content_text = "\n".join(
+            [f"({len(sons)}){typeprint_str(sons[0])[:-1]}" for sons in counter_values]
+        )
+        return (
+            add_padding("List[", current_padding)
+            + add_padding(
+                content_text,
+                current_padding + padding_increment * " ",
+            )
+            + add_padding("]", current_padding)
+        )
     return "Unknown Type"
 
 
-def typeprint(element: Any) -> None:
+def typeprint(*args, **kwargs) -> None:
     """Typeprint an element."""
-    print(typeprint_str(element))
+    print("\n".join(typeprint_str(*args, **kwargs)))
--- tests/test_typeprint.py
+++ tests/test_typeprint.py
@@ -4,11 +4,43 @@ from typing import Any
 
 import pytest
 
-from dprint.typeprint import typeprint_str
+from dprint.typeprint import add_padding, get_type, typeprint_str
 
 
 @pytest.mark.parametrize(
-    "test_input,expected", [(1, "int"), (-1, "int"), (1.2, "float"), ("2", "str")]
+    "test_input,current_padding,expected",
+    [
+        ("1", " " * 2, "  1\n"),
+        ("ab\ncd", " " * 2, "  ab\n  cd\n"),
+    ],
+)
+def test_add_padding(test_input: str, current_padding: str, expected: str):
+    """Test the add_padding() function."""
+    assert add_padding(test_input, current_padding=current_padding) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [(1, "int"), (1.2, "float"), ("d", "str")],
+)
+def test_get_type(test_input: Any, expected: str):
+    """Test the get_type() function."""
+
+    assert get_type(test_input) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        (1, "int\n"),
+        (-1, "int\n"),
+        (1.2, "float\n"),
+        ("2", "str\n"),
+        ([1, 2, 3], "List[\n  (3)int\n]\n"),
+        ([1, 2, 3, "abc"], "List[\n  (3)int\n  (1)str\n]\n"),
+        ([1, 2, 3, "abc", 3.5, 3.1], "List[\n  (2)float\n  (3)int\n  (1)str\n]\n"),
+        ([], "List[]\n"),
+    ],
 )
 def test_typeprint(test_input: Any, expected: str):
     """Test the typeprint_str() function."""
