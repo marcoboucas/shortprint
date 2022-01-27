6af3e314488b1f9a3f8fb417fc04b4e7887dcf55
Nicolas LAUBE <nicolas.laube@student-cs.fr>
2022-01-27 18:36:36 +0100

    tuple typer added

---
+++ shortprint/typers/tuple_typer.py
@@ -0,0 +1,40 @@
+"""List typer."""
+
+from collections import defaultdict
+from typing import Any, Callable, Tuple
+
+from shortprint.utils import add_padding, get_type
+
+
+def type_tuple(
+    *,
+    element: Tuple[Any],
+    recursive_func: Callable,
+    current_padding: str,
+    padding_increment: int,
+) -> str:
+    """Type for a list."""
+
+    # Empty list
+    if len(element) == 0:
+        return add_padding("Tuple[]", current_padding)
+
+    # If not, we count the number of elements that differ (in terms of type)
+    counter = defaultdict(list)
+    for ele in element:
+        counter[get_type(ele)].append(ele)
+    # Sort by type value (to get unique order)
+    counter_values = list(
+        map(lambda x: x[1], sorted(list(counter.items()), key=lambda x: x[0]))
+    )
+    content_text = "\n".join(
+        [f"({len(sons)}) {recursive_func(sons[0])[:-1]}" for sons in counter_values]
+    )
+    return (
+        add_padding("Tuple[", current_padding)
+        + add_padding(
+            content_text,
+            current_padding + padding_increment * " ",
+        )
+        + add_padding("]", current_padding)
+    )
--- tests/data.py
+++ tests/data.py
@@ -50,5 +50,11 @@ TEST_FOR_DICT = [
     ({}, "Dict[]\n"),
 ]
 
+TESTS_FOR_TUPLE = [
+    ((1, 2, 3), "Tuple[\n  (3) int\n]\n"),
+    ((1, 2, 3, "abc"), "Tuple[\n  (3) int\n  (1) str\n]\n"),
+    ((1, 2, 3, "abc", 3.5, 3.1), "Tuple[\n  (2) float\n  (3) int\n  (1) str\n]\n"),
+    ((), "Tuple[]\n"),
+]
 
 ALL_TESTS = [TESTS_FOR_LIST, TESTS_FOR_STANDARD, TEST_FOR_DICT, TESTS_FOR_DATACLASSES]
