b542a2d3779216773ea475563cb38c1141d324a5
Marco Boucas <marco.boucas@hotmail.fr>
2022-01-28 09:01:43 +0100

    Add sets

--- shortprint/typers/__init__.py
+++ shortprint/typers/__init__.py
@@ -3,6 +3,7 @@
 from .dataclasses_typer import type_dataclass
 from .dict_typer import type_dict
 from .list_typer import type_list
+from .set_typer import type_set
 from .tuple_typer import type_tuple
 
-__all__ = ["type_list", "type_dataclass", "type_dict", "type_tuple"]
+__all__ = ["type_list", "type_dataclass", "type_dict", "type_tuple", "type_set"]
